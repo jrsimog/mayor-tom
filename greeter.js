496e142c80c8f69798fcf6efd5dedc0d8e2f3992
Jose Simo <jsimo@ast.com.ve>
2017-09-29 10:49:49 -0400

    para inicializar el proyecto es necesario escribir el comoando [tsc - init],  *  el mismo crea un archivo de configuracion, una vez creado dicho archiuvo podemos ejecutar el comando [tsc *.tsc -w]  * que nos permite observar todos los archivos con extension .ts de nuestro proyecto inicializado

--- greeter.js
+++ greeter.js
@@ -1,4 +1,6 @@
 console.log('Hola Mundo');
-/*
-en consola se escribe el nombre del archivo en especifico que se quiere observar
-*/ 
+/**
+ * para inicializar el proyecto es necesario escribir el comoando [tsc - init],
+ *  el mismo crea un archivo de configuracion, una vez creado dicho archiuvo podemos ejecutar el comando [tsc *.tsc -w]
+ * que nos permite observar todos los archivos con extension .ts de nuestro proyecto inicializado
+ */ 
