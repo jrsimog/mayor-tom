ad6672400a75576d0b5077c8c73e5426b73d51f2
Jose Simo <jsimo@ast.com.ve>
2017-09-29 10:23:37 -0400

    Hola mundo

--- greeter.js
+++ greeter.js
@@ -1,8 +1,1 @@
-function desplegar(vengador) {
-    console.log("Desplegando a: " + vengador.nombre);
-}
-var thor = {
-    nombre: "Thor",
-    edad: 800
-};
-desplegar(thor);
+console.log('Hola Mundo');
--- greeter.ts
+++ greeter.ts
@@ -1,11 +1,2 @@
-class Avenger {
-    nombre:string;
-    arma:string;
-
-    constructor(nombre:string, arma:string){
-        this.nombre = nombre;
-        this.arma = arma;
-    }
-}
-
-let thor = new Avenger("Thor", "Mjolnir")
+console.log('Hola Mundo');
+alert('Hola Mundo');
