2dd2a2eb02f6eabe043f1e3375a6b38ab2b1a692
Jose Simo <jsimo@ast.com.ve>
2017-09-29 10:28:12 -0400

    en consola se escribe el nombre del archivo en especifico que se quiere observar file.ts  -w

--- greeter.js
+++ greeter.js
@@ -1,1 +1,4 @@
 console.log('Hola Mundo');
+/*
+en consola se escribe el nombre del archivo en especifico que se quiere observar
+*/ 
--- greeter.ts
+++ greeter.ts
@@ -1,2 +1,4 @@
 console.log('Hola Mundo');
-alert('Hola Mundo');
+/*
+en consola se escribe el nombre del archivo en especifico que se quiere observar
+*/
